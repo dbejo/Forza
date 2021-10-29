06a907d84bb5b16dde91224b8501a3b6e3b9e657
Dávid Bejó <bejodavid123@gmail.com>
2021-10-29 22:38:34 +0200

    Add project files.

---
+++ Forza/Forza.Designer.cs
@@ -0,0 +1,230 @@
+﻿
+namespace Forza
+{
+    partial class Forza
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1_max = new System.Windows.Forms.Label();
+            this.label2_min = new System.Windows.Forms.Label();
+            this.label3_front = new System.Windows.Forms.Label();
+            this.label4_back = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1_max = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2_min = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3_front = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown4_back = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1_max)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2_min)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3_front)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4_back)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1_max
+            // 
+            this.label1_max.AutoSize = true;
+            this.label1_max.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1_max.Location = new System.Drawing.Point(12, 13);
+            this.label1_max.Name = "label1_max";
+            this.label1_max.Size = new System.Drawing.Size(76, 32);
+            this.label1_max.TabIndex = 0;
+            this.label1_max.Text = "Max:";
+            // 
+            // label2_min
+            // 
+            this.label2_min.AutoSize = true;
+            this.label2_min.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2_min.Location = new System.Drawing.Point(12, 55);
+            this.label2_min.Name = "label2_min";
+            this.label2_min.Size = new System.Drawing.Size(69, 32);
+            this.label2_min.TabIndex = 1;
+            this.label2_min.Text = "Min:";
+            // 
+            // label3_front
+            // 
+            this.label3_front.AutoSize = true;
+            this.label3_front.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3_front.Location = new System.Drawing.Point(12, 97);
+            this.label3_front.Name = "label3_front";
+            this.label3_front.Size = new System.Drawing.Size(89, 32);
+            this.label3_front.TabIndex = 2;
+            this.label3_front.Text = "Front:";
+            // 
+            // label4_back
+            // 
+            this.label4_back.AutoSize = true;
+            this.label4_back.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4_back.Location = new System.Drawing.Point(12, 139);
+            this.label4_back.Name = "label4_back";
+            this.label4_back.Size = new System.Drawing.Size(86, 32);
+            this.label4_back.TabIndex = 3;
+            this.label4_back.Text = "Back:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(475, 97);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 32);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Front:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label6.Location = new System.Drawing.Point(475, 142);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(86, 32);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Back:";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(475, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(220, 71);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Count";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1_max
+            // 
+            this.numericUpDown1_max.Location = new System.Drawing.Point(105, 16);
+            this.numericUpDown1_max.Maximum = new decimal(new int[] {
+            99,
+            0,
+            0,
+            0});
+            this.numericUpDown1_max.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1_max.Name = "numericUpDown1_max";
+            this.numericUpDown1_max.Size = new System.Drawing.Size(120, 26);
+            this.numericUpDown1_max.TabIndex = 7;
+            this.numericUpDown1_max.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown2_min
+            // 
+            this.numericUpDown2_min.Location = new System.Drawing.Point(105, 58);
+            this.numericUpDown2_min.Maximum = new decimal(new int[] {
+            99,
+            0,
+            0,
+            0});
+            this.numericUpDown2_min.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown2_min.Name = "numericUpDown2_min";
+            this.numericUpDown2_min.Size = new System.Drawing.Size(120, 26);
+            this.numericUpDown2_min.TabIndex = 8;
+            this.numericUpDown2_min.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown3_front
+            // 
+            this.numericUpDown3_front.Location = new System.Drawing.Point(105, 100);
+            this.numericUpDown3_front.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown3_front.Name = "numericUpDown3_front";
+            this.numericUpDown3_front.Size = new System.Drawing.Size(120, 26);
+            this.numericUpDown3_front.TabIndex = 9;
+            // 
+            // numericUpDown4_back
+            // 
+            this.numericUpDown4_back.Location = new System.Drawing.Point(105, 142);
+            this.numericUpDown4_back.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown4_back.Name = "numericUpDown4_back";
+            this.numericUpDown4_back.Size = new System.Drawing.Size(120, 26);
+            this.numericUpDown4_back.TabIndex = 10;
+            // 
+            // Forza
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.numericUpDown4_back);
+            this.Controls.Add(this.numericUpDown3_front);
+            this.Controls.Add(this.numericUpDown2_min);
+            this.Controls.Add(this.numericUpDown1_max);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4_back);
+            this.Controls.Add(this.label3_front);
+            this.Controls.Add(this.label2_min);
+            this.Controls.Add(this.label1_max);
+            this.Name = "Forza";
+            this.Text = "Forza";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1_max)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2_min)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3_front)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4_back)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1_max;
+        private System.Windows.Forms.Label label2_min;
+        private System.Windows.Forms.Label label3_front;
+        private System.Windows.Forms.Label label4_back;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1_max;
+        private System.Windows.Forms.NumericUpDown numericUpDown2_min;
+        private System.Windows.Forms.NumericUpDown numericUpDown3_front;
+        private System.Windows.Forms.NumericUpDown numericUpDown4_back;
+    }
+}
+
---
+++ Forza/Forza.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Forza
+{
+    public partial class Forza : Form
+    {
+        public Forza()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double dampingf = 0;
+            double dampingb = 0;
+            double max = Decimal.ToDouble(numericUpDown1_max.Value);
+            double min = Decimal.ToDouble(numericUpDown2_min.Value);
+            double front = Decimal.ToDouble(numericUpDown3_front.Value) / 100;
+            double back = 1 - front;
+            numericUpDown4_back.Value = Convert.ToDecimal(back) * 100;
+            dampingf = (max - min) * front + min;
+            dampingb = (max - min) * back + min;
+            label5.Text = "Front: " + dampingf;
+            label6.Text = "Back: " + dampingb;
+        }
+    }
+}
